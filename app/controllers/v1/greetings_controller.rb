55b2b4cda9ba721726c40895a3ad7a6c5f18dce6
Nicholas Mutale <nicomutale@gmail.com>
2022-07-23 16:04:33 +0200

    Fix linter errors

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,27 +1,27 @@
 class V1::GreetingsController < ApplicationController
-    def index
-        render json: { greetings: [
-        {
-            name: 'Hello from Zambia',
-            guid: '0415674d-a0e8-4748-af68-a5f53a05c453'
-          },
-          {
-            name: 'Bonjour le monde',
-            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
-          },
-          {
-            name: 'Hola Mundo',
-            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
-          },
-          {
-            name: 'Ciao mondo',
-            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
-          },
-          {
-            name: 'Hallo Welt',
-            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
-          }
-    
-        ] }.to_json
-    end
+  def index
+    render json: { greetings: [
+      {
+        name: 'Hello from Zambia',
+        guid: '0415674d-a0e8-4748-af68-a5f53a05c453'
+      },
+      {
+        name: 'Bonjour le monde',
+        guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+      },
+      {
+        name: 'Hola Mundo',
+        guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+      },
+      {
+        name: 'Ciao mondo',
+        guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+      },
+      {
+        name: 'Hallo Welt',
+        guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+      }
+
+    ] }.to_json
+  end
 end
