3341a11c45640afd4e39fdca3735b6c6604eb617
Nicholas Mutale <nicomutale@gmail.com>
2022-07-23 15:21:55 +0200

    Set redux state store install redux thunk dev tools

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,10 +1,27 @@
 class V1::GreetingsController < ApplicationController
     def index
-        render json: {greetings: [
+        render json: { greetings: [
         {
-            name: 'Hello form Zambai'
-            guid: '123456789'
-        }
-    ] }.to_json
+            name: 'Hello from Zambia',
+            guid: '0415674d-a0e8-4748-af68-a5f53a05c453'
+          },
+          {
+            name: 'Bonjour le monde',
+            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+          },
+          {
+            name: 'Hola Mundo',
+            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+          },
+          {
+            name: 'Ciao mondo',
+            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+          },
+          {
+            name: 'Hallo Welt',
+            guid: '0415674d-a0e8-4748-af68-a5f53a05c445'
+          }
+    
+        ] }.to_json
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   root "static#index"
   namespace :v1, defaults: { format: :json } do
     get 'greetings', to: 'greetings#index'
+  end
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
 end
